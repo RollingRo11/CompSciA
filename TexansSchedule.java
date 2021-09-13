cf6850234c978f5a6d92951a467afbab2f9c61c9
RollingRo11 <55209318+RollingRo11@users.noreply.github.com>
2021-09-13 10:46:55 -0500

    Update and rename texansschedule.java to TexansSchedule.java

---
+++ TexansSchedule.java
@@ -0,0 +1,28 @@
+public class TexansSchedule{
+    public static void main(String[] args) {
+        System.out.println("            Houston Texans Schedule");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("Week    Day      Date                     Team");
+        System.out.println("Week  1 Sunday   09/12 vs Jacksonville Jaguars");
+        System.out.println("Week  2 Sunday   09/19 at     Cleveland Browns");
+        System.out.println("Week  3 Thursday 09/23 vs    Carolina Panthers");
+        System.out.println("Week  4 Sunday   10/03 at        Buffalo Bills");
+        System.out.println("Week  5 Sunday   10/10 vs New England Patriots");
+        System.out.println("Week  6 Sunday   10/17 at   Indianapolis Colts");
+        System.out.println("Week  7 Sunday   10/24 at    Arizona Cardinals");
+        System.out.println("Week  8 Sunday   10/31 vs     Los Angeles Rams");
+        System.out.println("Week  9 Sunday   11/07 at       Miami Dolphins");
+        System.out.println("Week 10                                    Bye");
+        System.out.println("Week 11 Sunday   11/21 at     Tennessee Titans");
+        System.out.println("Week 12 Sunday   11/28 vs        New York Jets");
+        System.out.println("Week 13 Sunday   12/05 vs   Indianapolis Colts");
+        System.out.println("Week 14 Sunday   12/12 vs     Seattle Seahawks");
+        System.out.println("Week 15 Sunday   12/19 at Jacksonville Jaguars");
+        System.out.println("Week 16 Sunday   12/26 vs Los Angeles Chargers");
+        System.out.println("Week 17 Sunday   01/02 at  San Francisco 49ers");
+        System.out.println("Week 18 Sunday   01/09 vs     Tennessee Titans");
+
+
+    }
+
+}
